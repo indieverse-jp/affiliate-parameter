af2ea1e96124f52e3cf311ac39843b6cdb67f7d3
shouya31 <shouya0331@ezweb.ne.jp>
2024-06-04 12:34:06 +0900

    Add query in redirect_to

--- affiliate-parameter.js
+++ affiliate-parameter.js
@@ -8,7 +8,8 @@
  * Date: 2023-09-20
  */
 
-var domainParamPairs = {
+(function () {
+  var domainParamPairs = {
     'presco.asia': 'afad_param_1',
     'moshimo.com': 's_v',
     'link-ag.net': 'sid',
@@ -32,30 +33,29 @@ var domainParamPairs = {
     //'m-ads.jp': 'uix',
     'aff.partners.io': 'afp',
   };
-  
-  var links = [].slice.call(document.getElementsByTagName('a'));
-  links.forEach(function(e) {
-    var url = e.href;
 
-    for (var domain in domainParamPairs) {
-      if (url.indexOf(domain) !== -1) {
+  var links = Array.from(document.getElementsByTagName('a'));
+
+  links.forEach(function (link) {
+    var url = link.href;
+
+    Object.keys(domainParamPairs).forEach(function (domain) {
+      if (url.includes(domain)) {
         var paramKey = domainParamPairs[domain];
         var redirectIndex = url.indexOf('?redirect_to=');
-        var newParam = paramKey + '=' + encodeURIComponent(document.URL);
+        var newParam = `${paramKey}=${encodeURIComponent(document.URL)}`;
+
         if (redirectIndex !== -1) {
           var baseUrl = url.substring(0, redirectIndex);
           var redirectParam = url.substring(redirectIndex);
-          var newUrl = baseUrl + '?' + newParam + '&' + redirectParam.substring(1); // remove the initial '?' from redirectParam
-          e.setAttribute('href', newUrl);
+          var newUrl = `${baseUrl}?${newParam}&${redirectParam.substring(1)}`;
+          link.setAttribute('href', newUrl);
         } else {
-          if (url.indexOf('?') === -1) {
-            url += '?' + newParam;
-          } else {
-            url += '&' + newParam;
-          }
-          e.setAttribute('href', url);
+          url += url.includes('?') ? `&${newParam}` : `?${newParam}`;
+          link.setAttribute('href', url);
         }
-        break;
+        return;
       }
-    }
+    });
   });
+})();
